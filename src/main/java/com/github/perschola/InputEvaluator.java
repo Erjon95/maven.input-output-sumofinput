6a5e035d64e3c94c3d5808747d322fccfc4d5195
perscholas-lhunter <lhunter@perscholas.org>
2019-12-02 13:24:37 -0500

    updated InputEvaluator & readme

--- src/main/java/com/github/perschola/InputEvaluator.java
+++ src/main/java/com/github/perschola/InputEvaluator.java
@@ -4,13 +4,13 @@ public class InputEvaluator {
     public void run() {
         // prompt user to input number
         // get user input
-        // create product variable
+        // create sum variable
 
         // create counter variable
         // evaluate input from user
-            // multiply counter by product
+            // add counter to sum
             // increment counter by 1
 
-        // display product to user
+        // display sum to user
     }
 }
