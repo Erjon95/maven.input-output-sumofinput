803d29714cabbea75ccbb727b7502001efd38b88
Erjon Dervishaj <er_djs@yahoo.com>
2019-12-03 22:09:14 -0500

    finished InputEvaluator

--- src/main/java/com/github/perschola/InputEvaluator.java
+++ src/main/java/com/github/perschola/InputEvaluator.java
@@ -1,16 +1,67 @@
 package com.github.perschola;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class InputEvaluator {
-    public void run() {
-        // prompt user to input number
-        // get user input
-        // create sum variable
+    private int sum, number;
+    private boolean exc;
 
-        // create counter variable
-        // evaluate input from user
-            // add counter to sum
-            // increment counter by 1
+    public InputEvaluator(){exc = true; sum = 0;}
 
-        // display sum to user
+    private boolean isExc() {
+        return exc;
+    }
+
+    private void setExc(boolean exc) {
+        this.exc = exc;
+    }
+
+    private int getNumber() {
+        return number;
+    }
+
+    private void setNumber(int number) {
+        this.number = number;
+    }
+
+    private int getSum() {
+        return sum;
+    }
+
+    private void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    private void readInputFromUser(Scanner scanner) {
+        while (isExc()) {
+            setExc(false);
+            try {
+                setNumber(scanner.nextInt());
+            } catch (InputMismatchException ime) {
+                System.out.println("Please, enter an integer");
+                setExc(true);
+                scanner.nextLine();
+            }
+        }
+    }
+
+    private int calculateSumOfInput()
+    {
+        for (int i = getNumber(); i >=0; i--)
+            setSum(getSum() + i);
+        return getSum();
+    }
+
+    protected void run() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please, enter an integer");
+
+        readInputFromUser(scanner);
+        System.out.println("The cumulative sum of " + number + " is " + calculateSumOfInput());
     }
 }
+
+
